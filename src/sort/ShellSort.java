6ee9fa23a608426c46249c2385bf2f27e3d8bde9
wqdchn <wqdong.chn@gmail.com>
2019-11-07 09:38:04 +0800

    希尔排序

--- src/sort/ShellSort.java
+++ src/sort/ShellSort.java
@@ -8,36 +8,33 @@ package sort;
  **/
 public class ShellSort {
 
-    public static void shellsort(int[] n){
-        int step = n.length / 2;
-        int i = 0;
-        int j = 0;
-        int temp = 0;
-        while (step >= 1) {//步长大于1时
-            for (i = step; i < n.length; i++) {
-                temp = n[i];//当前位置的值
-                j = i - step;
-                while (j >= 0 && n[j] > temp) {//距离当前位置步长为step的值
-                    n[j + step] = n[j];
-                    j = j - step;
-                }
-                n[j + step] = temp;
-                show(n);
-                System.out.println();
-            }
-            step = step / 2;
+  public static void shellsort(int[] n) {
+    int length = n.length;
+    int temp = 0;
+    for (int step = length / 2; step >= 1; step /= 2) { //步长大于1时
+      for (int i = step; i < length; i++) {
+        temp = n[i]; //当前位置的值
+        int j = i - step;
+        while (j >= 0 && n[j] > temp) { //距离当前位置步长为step的值
+          n[j + step] = n[j];
+          j = j - step;
         }
-
+        n[j + step] = temp;
+        show(n);
+        System.out.println("当前步长 = " + step);
+      }
     }
 
-    public static void show(int[] num){
-        for (int i = 0;i<num.length;i++){
-            System.out.print(num[i]+" ");
-        }
-    }
+  }
 
-    public static void main(String[] args){
-        int[] nums = {2,3,-2,11,40,23,-56,23,21};
-        shellsort(nums);
+  public static void show(int[] num) {
+    for (int i = 0; i < num.length; i++) {
+      System.out.print(num[i] + " ");
     }
+  }
+
+  public static void main(String[] args) {
+    int[] nums = {100, 3, -2, 11, 40, 23, -56, 23, 21};
+    shellsort(nums);
+  }
 }
