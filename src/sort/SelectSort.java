d6142611e776db715d2117c797e723ebbcbbcc7a
wqdchn <wqdong.chn@gmail.com>
2019-11-07 09:13:52 +0800

    选择排序

--- src/sort/SelectSort.java
+++ src/sort/SelectSort.java
@@ -8,31 +8,30 @@ package sort;
  **/
 public class SelectSort {
 
-    public static void selectsort(int[] nums){//选择排序，每次把最小值放到当前轮的第一位
-        for (int i=0;i<nums.length;i++){
-            int min = i;
-            for (int j=i+1;j<nums.length;j++){
-                if(nums[min]>nums[j]){
-                    min = j;
-                }
-            }
-            int temp = nums[i];
-            nums[i] = nums[min];
-            nums[min] = temp;
-            show(nums);
-            System.out.println("第"+i+"趟结果");
+  public static void selectsort(int[] nums) {//选择排序，每次把最小值放到当前轮的第一位，O(n^2)
+    for (int i = 0; i < nums.length - 1; i++) {
+      int min = i;
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[min] > nums[j]) {
+          min = j;
         }
+      }
+      int temp = nums[i];
+      nums[i] = nums[min];
+      nums[min] = temp;
+      show(nums);
+      System.out.println("第" + (i + 1) + "趟结果");
     }
+  }
 
-    public static void show(int[] num){
-        for (int i = 0;i<num.length;i++){
-            System.out.print(num[i]+" ");
-        }
+  public static void show(int[] num) {
+    for (int i = 0; i < num.length; i++) {
+      System.out.print(num[i] + " ");
     }
+  }
 
-    public static void main(String[] args){
-
-        int[] nums = {2,3,-2,11,40,23,-56,23,21};
-        selectsort(nums);
-    }
+  public static void main(String[] args) {
+    int[] nums = {2, 3, -2, 11, 40, 23, -56, 23, 21};
+    selectsort(nums);
+  }
 }
