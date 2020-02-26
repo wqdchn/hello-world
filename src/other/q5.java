5139a2dec3d8043ae14a5758124b2c767558b004
wqdchn <wqdong.chn@gmail.com>
2020-02-26 20:50:41 +0800

    逆序输出字符串

---
+++ src/other/q5.java
@@ -0,0 +1,26 @@
+package other;
+
+/**
+ * @program: hello-world
+ * @description: ÄæÐòÊä³ö×Ö·û´®
+ * @author: wqdong
+ * @create: 2020-02-26 20:38
+ **/
+public class q5 {
+
+  public static String reverseString(String str) {
+    char[] chars = str.toCharArray();
+    char[] rechars = new char[chars.length];
+    for (int i = 0; i < chars.length; i++) {
+      rechars[i] = chars[chars.length - i - 1];
+    }
+
+    return String.valueOf(rechars);
+  }
+
+  public static String reverseString2(String str){
+    StringBuilder sb = new StringBuilder(str);
+    return sb.reverse().toString();
+  }
+
+}
