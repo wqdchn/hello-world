432d4f1e41594331591f0d59eeebe62eaa88d143
wqdchn <wqdong.chn@gmail.com>
2021-01-17 22:18:21 +0800

    fix:Update 读取Excel文件生成INSERT语句

--- Python/file_to_sql.py
+++ Python/file_to_sql.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import os
 
-df = pd.read_excel(os.getcwd() + "/data/data.xlsx", 'Sheet1')
+df = pd.read_excel(os.getcwd() + '/data/data.xlsx', 'Sheet1')
 
 table_nm = '表名'
 
 sql_texts = []
 for index, row in df.iterrows():
+
     if index == 0:
         sql_texts.append(
-            'INSERT INTO ' + table_nm + ' (' + str(', '.join(df.columns)) + ')  VALUES ' + str(tuple(row.values)))
+            'INSERT INTO ' + table_nm + ' (' + str(','.join(df.columns)) + ')  VALUES ' + str(
+                tuple(str(row.values).replace('\'', '').strip('[').strip(']').split(' '))))
     else:
-        sql_texts.append(str(tuple(row.values)))
+        sql_texts.append(str(tuple(str(row.values).replace('\'', '').strip('[').strip(']').split(' '))))
 
-print(sql_texts)
+print(','.join(sql_texts))
