80c27087d19c12408f4e95cee39369433a721421
wqdchn <wqdong.chn@gmail.com>
2021-04-03 20:24:15 +0800

    update: md又学到了

--- Shell/shell_01.sh
+++ Shell/shell_01.sh
@@ -8,3 +8,10 @@ awk '{for (i = 1; i <= NF ; i++)  print $i}' txt/data.txt
 
 awk '{print $2}' txt/data.txt | sort | uniq -cd | sort
 
+awk -F, '{print NF}' test | uniq # 文件用逗号分割能形成几种列数
+
+grep $(printf '\r') test
+
+grep $(printf '\r') test | grep -o ','
+
+grep $(printf '\r') test | grep -o ',' | wc -l
